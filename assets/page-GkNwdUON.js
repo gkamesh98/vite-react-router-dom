09aafc42001f1f0ab69e106490e09620413fd174
gkamesh98 <gkamesh98@gmail.com>
2024-05-16 06:45:32 +0530

    Updates

---
+++ assets/page-GkNwdUON.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-SyU2C5vM.js";const r=()=>t.jsx("p",{children:"about"});export{r as default};
---
+++ assets/page-fnwN7zLP.js
@@ -0,0 +1,1 @@
+import{a as t,j as a}from"./index-SyU2C5vM.js";const o=()=>{const s=t();return console.log(s),a.jsx("p",{children:s.test})};export{o as default};
